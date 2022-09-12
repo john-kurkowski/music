c99367361051d822b23356ffab2d59704525ea4c
John Kurkowski <john.kurkowski@gmail.com>
2022-09-12 13:36:26 -0700

    Clarify doc

--- music/render.py
+++ music/render.py
@@ -20,7 +20,7 @@ def find_master_limiter_threshold(project: reapy.core.Project) -> reapy.core.FXP
 
 
 def set_param_value(param: reapy.core.FXParam, value: float) -> None:
-    """Work around bug with reapy's setter in 0.10."""
+    """Set a parameter's value. Work around bug with reapy 0.10's setter."""
     parent_fx = param.parent_list.parent_fx
     parent = parent_fx.parent
     param.functions["SetParamNormalized"](  # type: ignore[operator]
