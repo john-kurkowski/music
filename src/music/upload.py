685ec00985c13d9f862c9b79a3b2b3ef95183f12
John Kurkowski <john.kurkowski@gmail.com>
2022-09-12 00:38:04 -0700

    Upload file from disk

--- src/music/upload.py
+++ src/music/upload.py
@@ -27,14 +27,24 @@ def upload(oauth_token: str, files: list[Path]) -> None:
     )
     tracks_resp.raise_for_status()
 
-    filenames = {file.stem for file in files}
+    files_by_stem = {file.stem: file for file in files}
     tracks_by_title = {
         track["title"]: track
         for track in tracks_resp.json()["collection"]
-        if track["title"] in filenames
+        if track["title"] in files_by_stem
     }
-    missing_tracks = filenames.difference(tracks_by_title.keys())
+    missing_tracks = set(files_by_stem).difference(tracks_by_title.keys())
     if missing_tracks:
         raise KeyError(f"Tracks to upload not found in SoundCloud: {missing_tracks}")
 
-    # TODO: actually upload from disk
+    headers = {**headers, "Content-Type": "audio/wav"}
+    for stem, fil in files_by_stem.items():
+        track_id = tracks_by_title[stem]["id"]
+        with open(fil, "rb") as fobj:
+            upload_resp = requests.put(
+                f"https://api-v2.soundcloud.com/tracks/soundcloud:tracks:{track_id}",
+                data=fobj,
+                headers=headers,
+                timeout=60 * 10,
+            )
+            upload_resp.raise_for_status()
