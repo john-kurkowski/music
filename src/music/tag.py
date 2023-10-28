3e969df5290d1df237917b2fc7d9e4a3ea832533
John Kurkowski <john.kurkowski@gmail.com>
2023-10-28 01:56:21 -0700

    Only reencode newer source files

--- src/music/tag.py
+++ src/music/tag.py
@@ -7,6 +7,9 @@ from pathlib import Path
 def main(infile: Path) -> None:
     """Encode .wav file to .mp3 and tag with artist, album, and track number."""
     outfile = infile.with_suffix(".mp3")
+    if outfile.exists() and infile.stat().st_mtime < outfile.stat().st_mtime:
+        return
+
     artist = "Bluu"
     album = infile.parent.name
     num, _, title = infile.stem.partition(" - ")
