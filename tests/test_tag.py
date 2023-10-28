e293be2ec72cf72f7a1277c1f2bbc1a6decce495
John Kurkowski <john.kurkowski@gmail.com>
2023-10-28 01:16:30 -0700

    Test tag and encode

---
+++ tests/test_tag.py
@@ -0,0 +1,28 @@
+"""Tag and encode tests."""
+
+from pathlib import Path
+from unittest import mock
+
+from click.testing import CliRunner
+from music.__main__ import tag
+from syrupy.assertion import SnapshotAssertion
+
+
+@mock.patch("subprocess.check_call")
+def test_main(
+    mock_subprocess: mock.Mock, snapshot: SnapshotAssertion, tmp_path: Path
+) -> None:
+    """Test main calls the expected subprocess."""
+    some_path = (
+        tmp_path / "path" / "to" / "Album Title Here" / "01 - Song Title Here.wav"
+    )
+    some_path.parent.mkdir(parents=True, exist_ok=True)
+    some_path.touch()
+
+    result = CliRunner(mix_stderr=False).invoke(tag, [str(some_path)])
+
+    assert not result.stderr
+    assert not result.exception
+    assert result.stdout == snapshot
+
+    assert mock_subprocess.call_args == snapshot
