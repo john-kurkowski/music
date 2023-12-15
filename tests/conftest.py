a82dd7256b2a9e97ef2c76d68b30623ef431042b
John Kurkowski <john.kurkowski@gmail.com>
2023-12-15 15:30:54 -0800

    Strip verbose class `__repr__` from snapshot

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import re
 from pathlib import Path
 from typing import Any
 
+import aiohttp
 import pytest
 import syrupy
 from syrupy.assertion import SnapshotAssertion
@@ -25,7 +26,12 @@ def pytest_collection_modifyitems(
 def snapshot(
     monkeypatch: pytest.MonkeyPatch, snapshot: SnapshotAssertion, tmp_path: Path
 ) -> SnapshotAssertion:
-    """Override. Make syrupy's snapshot fixture strip temporary paths and tokens from any paths within a snapshot.
+    """Override. Make syrupy's snapshot fixture strip verbose class `__repr__`s, temporary paths, and temporary tokens from any paths within a snapshot.
+
+    Verbose class `__repr__`s can change between library versions, and we
+    usually don't care about 3rd party internals.
+
+    Temporary paths can change between test runs.
 
     In the case of console output, ensure no text wrapping occurs, so the
     regular expression matches.
@@ -35,8 +41,10 @@ def snapshot(
     tmp_path_str = str(tmp_path)
     tmp_id_re = re.compile(r"(?P<tmp_id>\s*\d+)(?P<ext>\.tmp)")
 
-    def without_tmp_path(data: Any, path: Any) -> Any:
-        if isinstance(data, Path) or isinstance(data, io.IOBase):
+    def matcher(data: Any, path: Any) -> Any:
+        if isinstance(data, aiohttp.ClientSession):
+            return "CLIENT_SESSION_HERE"
+        elif isinstance(data, Path) or isinstance(data, io.IOBase):
             return str(data).replace(tmp_path_str, "TMP_PATH_HERE")
         elif isinstance(data, str):
             without_tmp_id = tmp_id_re.sub(r"\g<ext>", data)
@@ -44,10 +52,12 @@ def snapshot(
             return without_path
         return data
 
-    class WithoutTmpPathExtension(syrupy.extensions.amber.AmberSnapshotExtension):
+    class WithoutVerboseAndTmpPathExtension(
+        syrupy.extensions.amber.AmberSnapshotExtension
+    ):
         def serialize(self, data: syrupy.types.SerializableData, **kwargs: Any) -> str:
             """Override."""
-            new_kwargs = kwargs | {"matcher": without_tmp_path}
+            new_kwargs = kwargs | {"matcher": matcher}
             return super().serialize(data, **new_kwargs)
 
-    return snapshot.use_extension(WithoutTmpPathExtension)
+    return snapshot.use_extension(WithoutVerboseAndTmpPathExtension)
