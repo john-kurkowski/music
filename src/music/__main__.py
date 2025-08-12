3a512676e4d49a9def92c8063eee8b33e5bc60e1
John Kurkowski <john.kurkowski@gmail.com>
2025-08-11 21:40:29 -0700

    Document import side-effect

--- src/music/__main__.py
+++ src/music/__main__.py
@@ -14,13 +14,21 @@ def cli() -> None:
     """Tasks for publishing my music."""
 
 
-for command_file in Path(__file__).parent.glob("commands/*/command.py"):
-    command_package = command_file.parent.name
-    command_module = importlib.import_module(
-        f"music.commands.{command_package}.command"
-    )
-    cli.add_command(command_module.main)
+def discover_commands() -> None:
+    """Discover CLI commands in this package.
 
+    Finds command modules by their conventional filename and location.
+    Registers them with the Click entrypoint, a top-level command group.
+    """
+    for command_file in Path(__file__).parent.glob("commands/*/command.py"):
+        command_package = command_file.parent.name
+        command_module = importlib.import_module(
+            f"music.commands.{command_package}.command"
+        )
+        cli.add_command(command_module.main)
+
+
+discover_commands()
 
 if __name__ == "__main__":  # pragma: no cover
     cli()
